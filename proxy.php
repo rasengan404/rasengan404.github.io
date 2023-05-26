3702eafae5a16135e4a1fa45d3899af929a53c16
rasengan404 <70688361+rasengan404@users.noreply.github.com>
2023-05-26 15:00:21 +0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,31 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+$code = $_POST['code'];
+$result = exec("python -c \"" . addslashes($code) . "\" 2>&1");
 
-require 'vendor/autoload.php';
-
-use GuzzleHttp\Client;
-
-$code = $_GET['code'];
-$url = 'https://api.jdoodle.com/v1/execute';
-$clientId = 'd6f56fb62c9cb394210a2c7425cc93c7';
-$clientSecret = '7126aeb78dce69e151ab4bb252d8b1b91b2933c6b5ffaadd73685a1401592ed';
-
-$client = new Client();
-
-$response = $client->post($url, [
-    'json' => [
-        'clientId' => $clientId,
-        'clientSecret' => $clientSecret,
-        'language' => 'python3',
-        'versionIndex' => 3,
-        'script' => $code
-    ],
-    'headers' => [
-        'Content-Type' => 'application/json'
-    ]
-]);
-
-$output = json_decode($response->getBody(), true)['output'];
-echo $output;
+echo json_encode(['output' => $result]);
 ?>
