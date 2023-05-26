e09e14c862dab24e84dfd1b999cc6b40d1d1d66a
rasengan404 <70688361+rasengan404@users.noreply.github.com>
2023-05-26 17:19:41 +0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,6 +1,13 @@
 <?php
 $code = $_GET['code'];
-$result = exec("python -c \"" . addslashes($code) . "\" 2>&1");
 
-echo json_encode(['output' => $result]);
+$command = 'python -c ' . escapeshellarg($code);
+
+exec($command, $output, $exitCode);
+
+if ($exitCode === 0) {
+    echo json_encode(['output' => implode("\n", $output)]);
+} else {
+    echo json_encode(['error' => implode("\n", $output)]);
+}
 ?>
