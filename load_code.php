27ef3df1bf652bfb47ccd0e6ef7dde7272e45d29
rasengan404 <70688361+rasengan404@users.noreply.github.com>
2023-06-14 13:11:08 +0300

    Create load_code.php

---
+++ load_code.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+
+    // Connect to SQLite database
+    $db = new SQLite3('db/codes.db');
+
+    // Retrieve code from database
+    $stmt = $db->prepare("SELECT code FROM codes WHERE id = :id");
+    $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+    $result = $stmt->execute();
+    $row = $result->fetchArray(SQLITE3_ASSOC);
+
+    if ($row) {
+        echo $row['code'];
+    }
+
+    // Close database connection
+    $db->close();
+}
+?>
