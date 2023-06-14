68f9dc5c304ed5335947e1bce75c4f75fa44df7d
rasengan404 <70688361+rasengan404@users.noreply.github.com>
2023-06-14 13:10:47 +0300

    Create save_code.php

---
+++ save_code.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $code = $_POST['code'];
+    $username = $_SESSION['username'];
+
+    // Connect to SQLite database
+    $db = new SQLite3('db/codes.db');
+
+    // Create codes table if it doesn't exist
+    $db->exec("CREATE TABLE IF NOT EXISTS codes (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, code TEXT)");
+
+    // Insert code into database
+    $stmt = $db->prepare("INSERT INTO codes (username, code) VALUES (:username, :code)");
+    $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+    $stmt->bindValue(':code', $code, SQLITE3_TEXT);
+    $stmt->execute();
+
+    $id = $db->lastInsertRowID();
+
+    echo 'Kods tika saglabāts ar ID: ' . $id;
+
+    // Close database connection
+    $db->close();
+}
+?>
