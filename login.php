1d3ecff9546c090f3a44015ff4a8f811567a9c5c
rasengan404 <70688361+rasengan404@users.noreply.github.com>
2023-06-14 13:09:21 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Connect to SQLite database
+    $db = new SQLite3('db/users.db');
+
+    // Check if username exists
+    $hashedPassword = $db->querySingle("SELECT password FROM users WHERE username='$username'");
+    if ($hashedPassword) {
+        // Verify password
+        if (password_verify($password, $hashedPassword)) {
+            echo 'Pieteikšanās veiksmīga.';
+        } else {
+            echo 'Nepareiza parole.';
+        }
+    } else {
+        echo 'Lietotājvārds nav atrasts.';
+    }
+
+    // Close database connection
+    $db->close();
+}
+?>
